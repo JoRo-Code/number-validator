402b89cb973df86af3c68fcfa359d91313cb365c
Jonas Rosengren <rosengrenjonas98@gmail.com>
2022-01-25 14:43:16 +0100

    parse() valid input
    - valid input tests
    - getters for number groups
    - toString()

--- src/validator/number/Number.java
+++ src/validator/number/Number.java
@@ -1,13 +1,76 @@
 package src.validator.number;
 
-public class Number {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public Number(String str) {
-        //TODO: implement
+public class Number {
+    private String century = "";
+    private String year = "";
+    private String month = "";
+    private String day = "";
+    private String delimiter = "";
+    private String last4 = "";
+
+    public final String getCentury() {
+        return this.century;
+    }
+    
+    public final String getYear() {
+        return this.year;
+    }
+    
+    public final String getMonth() {
+        return this.month;
+    }
+    
+    public final String getDay() {
+        return this.day;
+    }
+    
+    public final String getDelimiter() {
+        return this.delimiter;
+    }
+    
+    public final String getLast4() {
+        return this.last4;
+    }
+
+    private void parse(String str) {
+
+        String delimiters = "+-";
+        String pattern = "^(\\d{2})?(\\d{2})(\\d{2})(\\d{2})([" + delimiters + "]?)(\\d{4})$";
+
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(str);
+
+        if (m.find()) {
+            if (m.group(1) != null && !m.group(1).isEmpty()) {
+                this.century = m.group(1);
+            }
+            this.year = m.group(2);
+            this.month = m.group(3);
+            this.day = m.group(4);
+            this.delimiter = m.group(5);
+            this.last4 = m.group(6);
+        }
+        else {
+            throw new RuntimeException("Invalid input");
+        }
+
+
+    }
+
+    public Number(String number) {
+        parse(number);
     }
 
     public boolean isValid() {
         return false; //TODO: implement
+    }
+
+    @Override
+    public String toString() {
+        return this.century + this.year + this.month + this.day + this.delimiter + this.last4;
     }
     
 }
--- test/Test.java
+++ test/Test.java
@@ -1,5 +1,6 @@
 import src.validator.Validator;
 import src.validator.number.Persnumber;
+import src.validator.number.Number;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -21,6 +22,7 @@ public class Test {
     private static void printStatistics() {
         int failedTests = numberOfTests - passedTests;
         if(failedTests == 0) {
+            System.out.println("Passed " + passedTests + "/" + numberOfTests + " test" + (failedTests == 1? "":"s"));
             System.out.println(ANSI_GREEN + "All tests passed!" + ANSI_RESET);
         } else {
             System.out.println("Passed " + passedTests + "/" + numberOfTests + " tests");
@@ -56,6 +58,82 @@ public class Test {
         return null; //Cheat
     }
 
+    private static void parserTests() {
+    System.out.println("Parser tests");
+     // Valid
+
+        String number;
+        // 12
+        number = "201701102384";
+        Number n0 = new Number(number);
+        System.out.println(n0);
+        test(number.equals(n0.toString()), true);
+        test(n0.getCentury().equals("20"), true);
+        test(n0.getYear().equals("17"), true);
+        test(n0.getMonth().equals("01"), true);
+        test(n0.getDay().equals("10"), true);
+        test(n0.getDelimiter().equals(""), true);
+        test(n0.getLast4().equals("2384"), true);
+
+        // 12 w/ -
+        number = "20080903-2386";
+        Number n1 = new Number(number);
+        System.out.println(n1);
+        test(number.equals(n1.toString()), true);
+        test(n1.getCentury().equals("20"), true);
+        test(n1.getYear().equals("08"), true);
+        test(n1.getMonth().equals("09"), true);
+        test(n1.getDay().equals("03"), true);
+        test(n1.getDelimiter().equals("-"), true);
+        test(n1.getLast4().equals("2386"), true);
+        
+        // 10 w/ - 
+        number = "141206-2380";
+        Number n2 = new Number(number);
+        System.out.println(n2);
+        test(number.equals(n2.toString()), true);
+        test(n2.getCentury().equals(""), true);
+        test(n2.getYear().equals("14"), true);
+        test(n2.getMonth().equals("12"), true);
+        test(n2.getDay().equals("06"), true);
+        test(n2.getDelimiter().equals("-"), true);
+        test(n2.getLast4().equals("2380"), true);
+
+        // 10 w/ + 
+        number = "900118+9811";
+        Number n3 = new Number(number);
+        System.out.println(n3);
+        test(number.equals(n3.toString()), true);
+        test(n3.getCentury().equals(""), true);
+        test(n3.getYear().equals("90"), true);
+        test(n3.getMonth().equals("01"), true);
+        test(n3.getDay().equals("18"), true);
+        test(n3.getDelimiter().equals("+"), true);
+        test(n3.getLast4().equals("9811"), true);
+
+        // 10 
+        number = "9001189811";
+        Number n4 = new Number(number);
+        System.out.println(n4);
+        test(number.equals(n4.toString()), true);
+        test(n4.getCentury().equals(""), true);
+        test(n4.getYear().equals("90"), true);
+        test(n4.getMonth().equals("01"), true);
+        test(n4.getDay().equals("18"), true);
+        test(n4.getDelimiter().equals(""), true);
+        test(n4.getLast4().equals("9811"), true);
+    //Invalid
+        // Should throw an exception which can be caught and then evaluated
+    /* // TODO: 
+        // empty
+        Number n4 = new Number("");
+        // String
+        Number n5 = new Number("Hello");
+        // 10 w/ ?
+        Number n6 = new Number("900118?9811");
+    */
+    }
+
     private static void persnumberTests() {
         
         // valid
@@ -81,7 +159,8 @@ public class Test {
 
 
     private static void runTests() {
-        persnumberTests();
+        parserTests();
+        //persnumberTests();
         // TODO: add more tests
 
     } 
