abb379aefbcc2aaaa5bf5c24bf5b0d969f3c5609
Jonas Rosengren <rosengrenjonas98@gmail.com>
2022-01-26 17:43:28 +0100

    orgnumber format checker

--- src/validator/checks/OrgnumberFormatCheck.java
+++ src/validator/checks/OrgnumberFormatCheck.java
@@ -3,13 +3,25 @@ package src.validator.checks;
 import src.validator.number.Number;
 
 public class OrgnumberFormatCheck extends ValidityCheck {
+
+    private boolean isValidCentury(Number n) {
+        String c =  n.getCentury();
+
+        return c.equals("") || c.equals("16");
+
+    }
+
+    private boolean isValidMonth(Number n) {
+        String m = n.getMonth();
+        int month = Integer.parseInt(m);
+        return month >= 20;
+
+    }
     
     @Override
     public boolean run(Number n) {
 
-        return false; // TODO: implement
-        
-
+        return isValidCentury(n) && isValidMonth(n);
     }
     
 }
